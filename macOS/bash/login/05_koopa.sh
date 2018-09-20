f979af6e00c64a4186130766ec2c3afa71d49f44
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-20 16:32:49 -0400

    Reorganize

---
+++ macOS/bash/login/05_koopa.sh
@@ -0,0 +1,9 @@
+export CONDA_DIR="${HOME}/anaconda3/bin"
+
+# koopa shell
+# https://github.com/steinbaugh/koopa
+export KOOPA_DIR="${HOME}/koopa/bin"
+export PATH="${KOOPA_DIR}:${PATH}"
+source koopa
+
+# conda activate steinbaugh
--- macOS/bash/profile/05_koopa.sh
+++
@@ -1,9 +0,0 @@
-export CONDA_DIR="${HOME}/anaconda3/bin"
-
-# koopa shell
-# https://github.com/steinbaugh/koopa
-if [[ -n "$PS1" ]] && [[ -f ~/koopa/koopa.sh ]]; then
-    source ~/koopa/koopa.sh
-fi
-
-conda activate steinbaugh
