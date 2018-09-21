0cf5b793146c866717d8cf06c28e1d5c5e662218
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-20 21:24:04 -0400

    Simplify koopa install

--- macOS/bash/login/05_koopa.sh
+++ macOS/bash/login/05_koopa.sh
@@ -2,8 +2,7 @@ export CONDA_DIR="${HOME}/anaconda3/bin"
 
 # koopa shell
 # https://github.com/steinbaugh/koopa
-export KOOPA_DIR="${HOME}/koopa/bin"
-export PATH="${KOOPA_DIR}:${PATH}"
+export PATH="${HOME}/koopa/bin:${PATH}"
 source koopa
 
 # conda activate steinbaugh
