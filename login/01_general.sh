09fa2bbdd282b34c723d4ad496d4c731c7ee43aa
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-21 18:49:26 -0500

    Reorganize login scripts

--- login/01_general.sh
+++ login/01_general.sh
@@ -46,47 +46,7 @@ export PATH="/usr/local/sbin:${PATH}"
 # python -V
 # export PATH="/usr/local/opt/python/libexec/bin:${PATH}"
 
-# Set Builtin ==================================================================
-# Make bash stricter by default.
-# https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
-# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/
-
-# For scripts, this one-liner can be placed under the shebang line:
-# set -Eeuxo pipefail
-
-# Exit immediately when a command fails.
-set -e
-
-# Catch ERR traps properly, which won't fire in certain scenarios for `-e` flag.
-set -E
-
-# Treat unset variables as an error and exit immediately.
-set -u
-
-# Alternatively, can use:
-# shopt -s failglob
-
-# Print each command before executing. This command is great for scripts but too
-# noisy when added to bash_profile, and is not generally recommended here.
-# set -x
-
-# The bash shell normally only looks at the exit code of the last command of a
-# pipeline. This behavior is not ideal as it causes the `-e` option to only be
-# able to act on the exit code of a pipeline's last command. This is where
-# `-o pipefail` comes in. This particular option sets the exit code of a
-# pipeline to that of the rightmost command to exit with a non-zero status, or
-# to zero if all commands of the pipeline exit successfully.
-set -o pipefail
-
-# Use vi mode instead of emacs by default.
-set -o vi
-
-# Bash v4 checks for these unbound variables.
-EMACS=0
-INSIDE_EMACS=0
-
 # etc includes =================================================================
 # Enable bash completion.
 # This will fail if `set -u` is enabled.
-# [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
