8d467a905a07cadb162ab91c62cfc9cbdb77a78e
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-07 17:25:20 -0500

    Draft update to submodules

--- INSTALL/submodules-init.sh
+++ INSTALL/submodules-init.sh
@@ -4,19 +4,19 @@ set -Eeuo pipefail
 # Using pack (native Vim 8 package manager).
 # Installing dependencies as submodules.
 
-# These commands will modify `.gitmodules` file.
-git submodule add --force \
-    git@github.com:mjsteinbaugh/vim-airline.git \
-    pack/dist/start/vim-airline
-git submodule add --force \
-    git@github.com:mjsteinbaugh/vim-dracula.git \
-    pack/dist/start/dracula-theme
-git submodule add --force \
-    git@github.com:mjsteinbaugh/vim-markdown.git \
-    pack/dist/start/vim-markdown
-git submodule add --force \
-    git@github.com:mjsteinbaugh/Nvim-R.git \
-    pack/dist/start/Nvim-R
+# These commands were used to prepare `.gitmodules` file.
+# git submodule add --force \
+#     git@github.com:mjsteinbaugh/vim-airline.git \
+#     pack/dist/start/vim-airline
+# git submodule add --force \
+#     git@github.com:mjsteinbaugh/vim-dracula.git \
+#     pack/dist/start/dracula-theme
+# git submodule add --force \
+#     git@github.com:mjsteinbaugh/vim-markdown.git \
+#     pack/dist/start/vim-markdown
+# git submodule add --force \
+#     git@github.com:mjsteinbaugh/Nvim-R.git \
+#     pack/dist/start/Nvim-R
 
 # Initialize the submodules.
 git submodule init
