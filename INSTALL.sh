8bf3d48dd3ed859c43fdf98f9c02ff08d44d73bf
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-01 13:33:06 -0400

    Reorganize install scripts

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,23 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-# Initialize submodules.
-# Vim: using pack (native Vim 8 package manager).
-
-# These commands were used to prepare `.gitmodules` file.
-# git submodule add --force \
-#     git@github.com:mjsteinbaugh/vim-airline.git \
-#     pack/dist/start/vim-airline
-# git submodule add --force \
-#     git@github.com:mjsteinbaugh/vim-dracula.git \
-#     pack/dist/start/dracula-theme
-# git submodule add --force \
-#     git@github.com:mjsteinbaugh/vim-markdown.git \
-#     pack/dist/start/vim-markdown
-# git submodule add --force \
-#     git@github.com:mjsteinbaugh/Nvim-R.git \
-#     pack/dist/start/Nvim-R
-
 # Initialize the submodules.
 git submodule init
 
