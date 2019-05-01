b9f92e42826c7727d351f94ecd6e5786e59a9c0b
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-01 13:12:00 -0400

    Update submodules

--- submodules.sh
+++ submodules.sh
@@ -4,19 +4,20 @@
 # Dracula ======================================================================
 # Consolidate the Dracula color themes available on GitHub.
 
+
 # Vim ==========================================================================
 # Using pack, for native Vim 8 package manager.
 # This is now recommended instead of using pathogen.
 git submodule add \
     git@github.com:mjsteinbaugh/vim-airline.git \
-    pack/dist/start/vim-airline
+    vim/pack/dist/start/vim-airline
 git submodule add \
     git@github.com:mjsteinbaugh/vim-dracula.git \
-    pack/dist/start/dracula-theme
+    vim/pack/dist/start/dracula-theme
 git submodule add \
     git@github.com:mjsteinbaugh/vim-markdown.git \
-    pack/dist/start/vim-markdown
+    vim/pack/dist/start/vim-markdown
 git submodule add \
     git@github.com:mjsteinbaugh/Nvim-R.git \
-    pack/dist/start/Nvim-R
+    vim/pack/dist/start/Nvim-R
 
