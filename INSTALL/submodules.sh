354fde4c19801255ca71d3505d97f4971956f892
Michael Steinbaugh <mike@steinbaugh.com>
2019-06-04 11:53:17 -0400

    Switch to HTTPS for submodules

--- INSTALL/submodules.sh
+++ INSTALL/submodules.sh
@@ -7,25 +7,25 @@ set -Eeuxo pipefail
 # Dracula ======================================================================
 # Consolidate the Dracula color themes available on GitHub.
 git submodule add \
-    git@github.com:mjsteinbaugh/dracula-putty.git \
+    https://github.com:mjsteinbaugh/dracula-putty.git \
     dracula/putty
 git submodule add \
-    git@github.com:mjsteinbaugh/dracula-powershell.git \
+    https://github.com:mjsteinbaugh/dracula-powershell.git \
     dracula/powershell
 
 # Vim ==========================================================================
 # Using pack, for native Vim 8 package manager.
 # This is now recommended instead of using pathogen.
 git submodule add \
-    git@github.com:mjsteinbaugh/vim-airline.git \
+    https://github.com:mjsteinbaugh/vim-airline.git \
     vim/pack/dist/start/vim-airline
 git submodule add \
-    git@github.com:mjsteinbaugh/vim-dracula.git \
+    https://github.com:mjsteinbaugh/vim-dracula.git \
     vim/pack/dist/start/dracula-theme
 git submodule add \
-    git@github.com:mjsteinbaugh/vim-markdown.git \
+    https://github.com:mjsteinbaugh/vim-markdown.git \
     vim/pack/dist/start/vim-markdown
 git submodule add \
-    git@github.com:mjsteinbaugh/Nvim-R.git \
+    https://github.com:mjsteinbaugh/Nvim-R.git \
     vim/pack/dist/start/Nvim-R
 
