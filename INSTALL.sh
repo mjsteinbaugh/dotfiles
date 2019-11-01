b7ec7585045abed9820b61e3c5d1c473a89c02d9
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-01 16:10:19 -0400

    Draft update to installer

--- INSTALL.sh
+++ INSTALL.sh
@@ -13,6 +13,40 @@ rm -fr ~/.emacs.d-spacemacs
 rm -fr ~/.oh-my-zsh
 rm -fr ~/.shrc
 
+# Always create these dotfiles.
+link-dotfile --force "condarc"
+link-dotfile --force "gitignore"
+link-dotfile --force "spacemacs"
+link-dotfile --force "tmux.conf"
+link-dotfile --force "vim"
+link-dotfile --force "vimrc"
+
+# FIXME Rethink this approach.
+
+# Create local config files.
+if [[ "$shared" -eq 0 ]]
+then
+    host_type="$(_koopa_host_type)"
+    os_type="$(_koopa_os_type)"
+    link-dotfile --force "Rprofile"
+    link-dotfile --force "bash_profile"
+    link-dotfile --force "bashrc"
+    link-dotfile --force "kshrc"
+    link-dotfile --force "shrc"
+    link-dotfile --force "zshrc"
+    # R
+    if [[ "$os_type" == "darwin" ]]
+    then
+        link-dotfile --force "os/darwin/R"
+    elif [[ "$host_type" == "harvard-o2" ]]
+    then
+        link-dotfile --force "host/harvard-o2/Renviron"
+    elif [[ "$host_type" == "harvard-odyssey" ]]
+    then
+        link-dotfile --force "host/harvard-odyssey/Renviron"
+    fi
+fi
+
 link-dotfile --force --config app/emacs/doom/config.d doom
 link-dotfile --force --config app/htop
 link-dotfile --force --config app/neofetch
@@ -38,6 +72,12 @@ then
     link-dotfile --force os/darwin/app/git/gitconfig
 else
     link-dotfile --force app/git/gitconfig
+fi
+
+if [[ "$mike" -eq 1 ]]
+then
+    link-dotfile --force Rprofile
+    link-dotfile --force forward
 fi
 
 link-emacs spacemacs
