5252cee65115d5b31d67fa4e28b724b62e533100
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-01 11:24:40 -0400

    Add submodule re-init script

--- git-add-submodules.sh
+++ git-add-submodules.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 set -Eeu -o pipefail
 
+# > git config -f ".gitmodules" --list
+
 # See also:
 # - https://stackoverflow.com/a/11258810/3911732
 # - https://github.com/git/git/blob/master/git-submodule.sh
-# - https://github.com/git/git/blob/a3fbb2350dcd2d843d1d90b663521222aceb25fe/git-submodule.sh#L132
+# - https://github.com/git/git/blob/a3fbb2350dcd2d843d1d90b663521222aceb25fe/
+#       git-submodule.sh#L132
 
-modules="$(git config -f .gitmodules --get-regexp '^submodule\..*\.path$')"
+mapfile -t path_arr \
+    < <(git config -f ".gitmodules" --get-regexp '^submodule\..*\.path$')
 
-# FIXME READ INTO ARRAY
-# modules=("$modules")
+echo "${#path_arr[@]} submodules detected."
 
-
-while read -r path_key path
+for path_key in "${path_arr[@]}"
 do
+    path="$(echo "$path_key" | cut -d ' ' -f 2)"
+    path_key="$(echo "$path_key" | cut -d ' ' -f 1)"
     url_key=$(echo $path_key | sed 's/\.path/.url/')
-    url=$(git config -f .gitmodules --get "$url_key")
-    echo "$url"
-    echo "$path"
-    # git submodule add $url $path
+    url="$(git config -f ".gitmodules" --get "$url_key")"
+    git submodule add --force "$url" "$path"
 done
