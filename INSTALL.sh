57622e9e236307d0e349c0b88512fc33b363db41
Michael Steinbaugh <mike@steinbaugh.com>
2019-11-04 10:36:18 -0500

    Improve linkage

--- INSTALL.sh
+++ INSTALL.sh
@@ -13,31 +13,17 @@ rm -fr ~/.emacs.d-spacemacs
 rm -fr ~/.oh-my-zsh
 rm -fr ~/.shrc
 
-# Always create these dotfiles.
-link-dotfile --force "condarc"
-link-dotfile --force "gitignore"
-link-dotfile --force "spacemacs"
-link-dotfile --force "tmux.conf"
-link-dotfile --force "vim"
-link-dotfile --force "vimrc"
-
-# FIXME Rethink this approach.
-
 # Create local config files.
-if [[ "$shared" -eq 0 ]]
+if ! koopa_is_shared
 then
+    link-dotfile --force shell/bash/bash_profile
+    link-dotfile --force shell/bash/bashrc
+    link-dotfile --force shell/zsh/zshrc
     host_type="$(_koopa_host_type)"
-    os_type="$(_koopa_os_type)"
-    link-dotfile --force "Rprofile"
-    link-dotfile --force "bash_profile"
-    link-dotfile --force "bashrc"
-    link-dotfile --force "kshrc"
-    link-dotfile --force "shrc"
-    link-dotfile --force "zshrc"
-    # R
-    if [[ "$os_type" == "darwin" ]]
+    if _koopa_is_darwin
     then
-        link-dotfile --force "os/darwin/R"
+        mkdir -pv "${HOME}/.R"
+        ln -fnsv "${KOOPA_HOME}/os/darwin/etc/R/Makevars" "${HOME}/.R/."
     elif [[ "$host_type" == "harvard-o2" ]]
     then
         link-dotfile --force "host/harvard-o2/Renviron"
@@ -74,10 +60,7 @@ else
     link-dotfile --force app/git/gitconfig
 fi
 
-if [[ "$mike" -eq 1 ]]
+if [[ "${mike:-}" -eq 1 ]]
 then
-    link-dotfile --force Rprofile
-    link-dotfile --force forward
+    link-dotfile --force r/Rprofile
 fi
-
-link-emacs spacemacs
