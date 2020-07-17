cbd1568dae90107949af9e13190515abad4e29b5
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-17 13:31:49 -0400

    Update Brewfile

---
+++ os/darwin/app/homebrew/bundle-install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+brew bundle install \
+    --file='Brewfile' \
+    --no-lock \
+    --no-upgrade
---
+++ os/darwin/app/homebrew/dump-draft.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+brew bundle dump \
+    --file='Brewfile-draft' \
+    --force
