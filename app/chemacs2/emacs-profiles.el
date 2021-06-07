8400c34d54a1133a229e66746be793d9b5c352b0
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-07 14:09:52 -0400

    Improve Emacs config

---
+++ app/chemacs2/emacs-profiles.el
@@ -0,0 +1,3 @@
+(("default"   . ((user-emacs-directory . "~/.local/share/spacemacs")))
+ ("doom" . ((user-emacs-directory . "~/.local/share/doom")))
+ ("spacemacs"   . ((user-emacs-directory . "~/.local/share/spacemacs"))))
