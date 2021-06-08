be71e284a924fc9b084c7f3b9c5a4b90ac061288
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-08 11:36:02 -0400

    Improve alias handling and emacs config

--- app/chemacs/emacs-profiles.el
+++ app/chemacs/emacs-profiles.el
@@ -1,3 +1,4 @@
 (("default"   . ((user-emacs-directory . "~/.local/share/spacemacs")))
  ("doom" . ((user-emacs-directory . "~/.local/share/doom")))
+ ("prelude" . ((user-emacs-directory . "~/.local/share/prelude")))
  ("spacemacs"   . ((user-emacs-directory . "~/.local/share/spacemacs"))))
